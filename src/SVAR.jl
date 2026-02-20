4d47c941ba8348656c8550beb03cc65b27df74d4
emsabaderi <emad.saberi@hec.ca>
2026-02-19 19:21:31 -0500

    Update SVAR.jl

--- src/SVAR.jl
+++ src/SVAR.jl
@@ -1,48 +1,39 @@
-mutable struct SVAR{T} <: EmpiricalModel
-    Yfull::Matrix{T}
-    p::Int
-
-    Y::Matrix{T}
-    X::Matrix{T}
-    k::Int
-    T::Int
-    Te::Int
-
-    Phi::Union{Nothing,Matrix{T}}
-    Sigma::Union{Nothing,Matrix{T}}
-    # coeftable::Union{Nothing,DataFrames.DataFrame}
-    E::Union{Nothing,Matrix{T}}
-
-    Phi_blocks::Union{Nothing,Vector{Matrix{T}}}
-    F::Union{Nothing,Matrix{T}}
-    Q::Union{Nothing,Matrix{T}}
-
-    A::Union{Nothing,Matrix{T}}
-    C1::Union{Nothing,Matrix{T}}
-    scheme::Symbol
+mutable struct SVAR{T<:Real,AType<:AbstractMatrix{T}} <: EmpiricalModel
+    var::VAR{T}
+    A::AType
+    impact::Union{Nothing,Matrix{T}}  # structural impact matrix (A^{-1}, A^{-1}P, etc.)
 end
 
-function SVAR(Yfull::AbstractMatrix{T}, A::AbstractMatrix{T}; p::Int, scheme::Symbol=:none) where {T<:Real}
-    Y, X = lagmatrix(Yfull, p)
-    T, k = size(Yfull)
-
-
-function SVAR(var::VAR{T}; scheme::Symbol=:none) where {T}
-    C1 = nothing
-    if scheme == :none
-        A = nothing
-    elseif scheme == :short_run
-        A = cholesky(var.Sigma).L
-    elseif scheme == :long_run
-        C1 = longrun_impact(var)
-        A = cholesky(C1 * var.Sigma * C1').L
-    else
-        error("Unknown SVAR identification scheme: $scheme")
-    end
-
-    return SVAR{T}(var, A, C1, scheme)
+function SVAR(var::VAR{T}, A::AbstractMatrix) where {T}
+    Afloat = Array{T}(A)
+    SVAR{T,typeof(Afloat)}(var, Afloat, nothing)
 end
 
-function SVAR(var::VAR{T}, A::Matrix{T}; scheme::Symbol=:user) where {T}
-    return SVAR{T}(var, A, nothing, scheme)
+function SVAR(Yfull::AbstractMatrix{T}, p::Int, names, A::AbstractMatrix) where {T}
+    var = VAR(Yfull, p, names)
+    estimate!(var)
+    companion!(var)
+    autocov!(var)
+    autocorr!(var)
+    SVAR(var, A)
 end
+
+function SVAR(Ytable, p::Int, A::AbstractMatrix) where {T}
+    Tables.istable(Ytable) ||
+        throw(ArgumentError("Yfull must satisfy the Tables.jl interface"))
+
+    Yfull = Tables.matrix(Ytable)
+    names = Tables.columnnames(Ytable) .|> Symbol
+    SVAR(Yfull, p, names, A)
+end
+
+Phi(s::SVAR) = s.var.Phi
+Sigma(s::SVAR) = s.var.Sigma
+E(s::SVAR) = s.var.E
+F(s::SVAR) = s.var.F
+Q(s::SVAR) = s.var.Q
+Y(s::SVAR) = s.var.Y
+X(s::SVAR) = s.var.X
+k(s::SVAR) = s.var.k
+p(s::SVAR) = s.var.p
+t(s::SVAR) = s.var.t
